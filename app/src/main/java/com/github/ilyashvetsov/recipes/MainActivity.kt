5e8f530e71c5d3a34454356f939084a6adccb05d
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-04-05 14:57:43 +0300

    RA-04 fixed

--- app/src/main/java/com/github/ilyashvetsov/recipes/MainActivity.kt
+++ app/src/main/java/com/github/ilyashvetsov/recipes/MainActivity.kt
@@ -1,37 +1,25 @@
 package com.github.ilyashvetsov.recipes
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.github.ilyashvetsov.recipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        R.layout.activity_main
         if (savedInstanceState == null) {
-            val categoriesFragment = CategoriesListFragment()
-            supportFragmentManager.beginTransaction().commit {
-                addFragment(R.id.mainContainer, categoriesFragment)
+            supportFragmentManager.commit {
                 setReorderingAllowed(true)
+                add<CategoriesListFragment>(R.id.mainContainer)
             }
+
         }
     }
-}
-
-
-fun FragmentTransaction.addFragment(containerId: Int, fragment: Fragment) {
-    this.add(containerId, fragment)
-}
-
-fun FragmentTransaction.commit(block: FragmentTransaction.() -> Unit) {
-    block()
-    commit()
 }
 
