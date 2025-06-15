b4ee967fa380dd5e80ccae4abba4a48ee997c37d
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-06-15 20:33:57 +0300

    RA-20 done

--- app/src/main/java/com/github/ilyashvetsov/recipes/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/github/ilyashvetsov/recipes/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,9 +1,15 @@
 package com.github.ilyashvetsov.recipes.ui.recipes.recipe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.ilyashvetsov.recipes.ui.FavoritesFragment
 
 class RecipeViewModel : ViewModel() {
+    private val _screenState: MutableLiveData<RecipeScreenState> =
+        MutableLiveData(RecipeScreenState())
+    val screenState: LiveData<RecipeScreenState> = _screenState
+
     data class RecipeScreenState(
         val ingredientsList: List<String> = listOf(),
         val methodList: List<String> = listOf(),
