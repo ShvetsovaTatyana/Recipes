d17e76219f133b7edf0744c5cc97b5b6d976c2d9
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-06-15 20:37:12 +0300

    RA-20 done

--- app/src/main/java/com/github/ilyashvetsov/recipes/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/github/ilyashvetsov/recipes/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,11 +1,14 @@
 package com.github.ilyashvetsov.recipes.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
+import com.github.ilyashvetsov.recipes.ui.FavoritesFragment
 
 class RecipeViewModel : ViewModel() {
     data class RecipeScreenState(
         val ingredientsList: List<String> = listOf(),
-        val methodList: List<String> = listOf()
+        val methodList: List<String> = listOf(),
+        val isFavorite: Boolean = false,
+        val numberServings: Int = 1
     )
 }
 
